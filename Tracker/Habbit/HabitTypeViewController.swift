cbe96677ba0dd70f3c197eacc8dde026353a8952
Alex Agafonov <agafonov.alexander@gmail.com>
2025-07-06 23:44:26 +0300

    Добавление persistentContainer и создание класса с типом привычки

---
+++ Tracker/Habbit/HabitTypeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HabitTypeViewController.swift
+//  Tracker
+//
+//  Created by Alexander Agafonov on 06.07.2025.
+//
+
+import UIKit
+
+final class HabitTypeViewController: UIViewController {
+    
+}
